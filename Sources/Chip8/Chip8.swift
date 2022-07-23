fa4af028edca2aeb690f5bedd024fd981229ce28
Thomas Bonk <thomas@meandmymac.de>
2022-07-23 12:04:14 +0200

    Initial Commit

---
+++ Sources/Chip8/Chip8.swift
@@ -0,0 +1,6 @@
+public struct Chip8 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/Chip8Tests/Chip8Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Chip8
+
+final class Chip8Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Chip8().text, "Hello, World!")
+    }
+}
